2d39d32ef3809623613c595eb0d0e22a615644f6
Jordan Bobo <bobojo@gmail.com>
2013-03-07 08:51:20 -0700

    Working on new anything slider for responsive

--- js/init.js
+++ js/init.js
@@ -5,7 +5,7 @@
 $LAB
   .script("http://byuweb.github.com/Global-Assets/js/jquery-1.9.1.min.js").wait()
   .script("http://byuweb.github.com/Global-Assets/js/jquery-migrate-1.1.1.min.js").wait()
-  .script("js/script.min.js"); }
+  .script("js/script.min.js");
 
 if(Modernizr.fontface){
   $LAB
