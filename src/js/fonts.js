823862fe7b94760ca5904eea0b47ba7d39dd62d9
Nathan Walton <nwalton@byu.edu>
2013-06-08 08:49:00 -0600

    Self-hosted fonts, loaded by Google font loader

--- src/js/fonts.js
+++ src/js/fonts.js
@@ -1,11 +1,4 @@
 
-// /* Load specific fonts */
-// WebFont.load({
-//         google: {
-//             families: ["Open+Sans:400italic,600italic,400,600:latin", "Crete+Round:400,400italic:latin", 'Droid+Serif:latin']
-//         }
-//     });
-
 function progress(message) {
   var output = document.getElementById('events');
   if (output) {
@@ -21,8 +14,33 @@ function progress(message) {
 
 WebFont.load({
   custom: {
-    families: ['ChunkFiveRegular'],
-    urls : ['http://seanmcb.com/typekit/wfl/stylesheet.css']
+    families: ['OpenSans:n4,i4,n5,i5', 'CreteRound:n4,i4', 'DroidSerif:n4'],
+    urls : ['../css/fonts.css']
+  },
+  loading: function() {
+    progress('loading');
+  },
+  active: function() {
+    progress('active');
+  },
+  inactive: function() {
+    progress('inactive');
+  },
+  fontloading: function(fontFamily, fontDescription) {
+    progress('fontloading: ' + fontFamily + ' (' + fontDescription + ')');
+  },
+  fontactive: function(fontFamily, fontDescription) {
+    progress('fontactive: ' + fontFamily + ' (' + fontDescription + ')');
+  },
+  fontinactive: function(fontFamily, fontDescription) {
+    progress('fontinactive: ' + fontFamily + ' (' + fontDescription + ')');
+  }
+});
+
+WebFont.load({
+  custom: {
+    families: ['FontAwesome'],
+    urls : ['../css/fonts.css']
   },
   loading: function() {
     progress('loading');
