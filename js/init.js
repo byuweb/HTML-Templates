370b1bdab22a56863de68c68e1d56ebb1b6e7570
Ephraim <ephraim.sng@gmail.com>
2013-06-18 12:30:14 -0600

    Fixed scripts for slider
    We were using LabJS which required to use their loading process instead
    of sequential html load. See http://labjs.com/documentation.php

--- js/init.js
+++ js/init.js
@@ -2,9 +2,9 @@
 
 $LAB
   .script("http://byuweb.github.io/Global-Assets/js/jquery-1.9.1.min.js").wait()
-  .script("http://byuweb.github.io/Global-Assets/js/jquery-migrate-1.1.1.min.js").wait()
+  .script("http://byuweb.github.io/Global-Assets/js/jquery-migrate-1.1.1.min.js")
   .script("js/script.min.js");
 
-if(Modernizr.fontface){
-  $LAB
-    .script("js/fonts.min.js"); }
+//if(Modernizr.fontface){
+//  $LAB
+//    .script("js/fonts.min.js"); }
