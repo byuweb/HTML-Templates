fe389c98bf766b0bfcd2d75071e95a71de7e283a
Nathan Walton <nwalton@byu.edu>
2013-06-27 17:57:34 -0600

    Make separate script file for touch-enabled devices

--- js/init.js
+++ js/init.js
@@ -6,21 +6,21 @@
 		http://modernizr.com/docs/#load
 		http://yepnopejs.com/
 */
-
-Modernizr.load([
-	// Load jQuery first
-	{
-		load: 'http://byuweb.github.io/Global-Assets/js/jquery-1.9.1.min.js',
-		complete: function () {
-			if ( !window.jQuery ) {
-				Modernizr.load('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js');
-			}
-		}
-	},
-	// Load scripts required by jQuery second. Load fonts script only if web fonts are supported
-	{
-		test : Modernizr.fontface,
-		load: "js/script.min.js",
-		yep : "js/fonts.min.js"
-	}
-]);
+Modernizr.load([ // Load jQuery first
+{
+    load: "http://byuweb.github.io/Global-Assets/js/jquery-1.9.1.min.js",
+    complete: function() {
+        if (!window.jQuery) {
+            Modernizr.load("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js");
+        }
+    }
+}, // Load scripts required by jQuery second. Load fonts script only if web fonts are supported
+{
+    test: Modernizr.touch,
+    nope: "js/script.min.js",
+    yep: "js/script-touch.min.js"
+}, // Load fonts if fonts are supported
+{
+    test: Modernizr.fontface,
+    yep: "js/fonts.min.js"
+} ]);
