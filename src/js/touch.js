1caf1c1257441ddd97e2916c8d951e3014d0a2af
Nathan Walton <nwalton@byu.edu>
2013-07-02 17:42:14 -0600

    Begin to add swipe events for touchscreen

--- src/js/touch.js
+++ src/js/touch.js
@@ -1,6 +1,30 @@
 /* touch.js */
 
 $(function() {
-    FastClick.attach(document.body);
+	//FastClick.attach(document.body);
+
+	document.addEventListener('touchmove', function(e) {
+	    e.preventDefault();
+	    var touch = e.touches[0];
+	    //log(touch.pageX + " / " + touch.pageY);
+	}, false);
+
+	$('body').on('swipeleft', function(e){
+		log('Swipe left');
+	});
+	$(window).on('swiperight', function(e){
+		log('Swipe right');
+	});
+
+	$(window).on('movestart', function(e) {
+		log('movestart');
+		// If the movestart is heading off in an upwards or downwards
+		// direction, prevent it so that the browser scrolls normally.
+		if ((e.distX > e.distY && e.distX < -e.distY) || (e.distX < e.distY && e.distX > -e.distY)) {
+			
+			e.preventDefault();
+		}
+	});
+
 });
 
