6fdc0eb855fca8ea97b5d031feb8d9c91b99ecbf
Nathan Walton <nwalton@byu.edu>
2013-07-10 17:21:20 -0600

    Add swipe to slider

--- src/js/touch.js
+++ src/js/touch.js
@@ -50,5 +50,61 @@ $(function() {
 		 threshold:swipewidth
 	});
 
+	// If a slider is present, enable swipe events
+	if( typeof($.anythingSlider) == 'function') {
+		$('#slider').anythingSlider({
+		    // Callback when the plugin finished initializing
+		    onInitialized: function(e, slider) {
+		        setupSwipe(slider);
+		    }
+		});
+	}
 });
+
+
+/******************************************
+ Swipe Demo - without using jQuery Mobile
+ ******************************************/
+var setupSwipe = function(slider) {
+    var time = 1000,
+        // allow movement if < 1000 ms (1 sec)
+        range = 50,
+        // swipe movement of 50 pixels triggers the slider
+        x = 0,
+        t = 0,
+        touch = "ontouchend" in document,
+        st = (touch) ? 'touchstart' : 'mousedown',
+        mv = (touch) ? 'touchmove' : 'mousemove',
+        en = (touch) ? 'touchend' : 'mouseup';
+
+    slider.$window
+        .bind(st, function(e) {
+            // prevent image drag (Firefox)
+            e.preventDefault();
+            t = (new Date()).getTime();
+            x = e.originalEvent.touches ? e.originalEvent.touches[0].pageX : e.pageX;
+        })
+        .bind(en, function(e) {
+            t = 0;
+            x = 0;
+        })
+        .bind(mv, function(e) {
+            e.preventDefault();
+            var newx = e.originalEvent.touches ? e.originalEvent.touches[0].pageX : e.pageX,
+                r = (x === 0) ? 0 : Math.abs(newx - x),
+                // allow if movement < 1 sec
+                ct = (new Date()).getTime();
+            if (t !== 0 && ct - t < time && r > range) {
+                if (newx < x) {
+                    slider.goForward();
+                }
+                if (newx > x) {
+                    slider.goBack();
+                }
+                t = 0;
+                x = 0;
+            }
+        });
+};
+
 
