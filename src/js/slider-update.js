ac061e2863f97c2fb54a143e6cc61a830ef5ba3b
Nathan Walton <nwalton@byu.edu>
2013-07-31 18:02:40 -0600

    Move initialization to one function

--- src/js/slider-update.js
+++ src/js/slider-update.js
@@ -18,47 +18,18 @@
 	// On page Load
 	$(function(){
 
-		// load image function
-		var loadImg = function(slider, page) {
-			slider.$items.eq(page).find('img').each(function() {
-				if ($(this).attr('src') === '') {
-					var newsrc = $(this).attr('data-src');
-					$(this).attr('src', newsrc);
-				}
-			});
-			$('#slider').anythingSlider();
-		};
+		initializeSlider();
+
+	});
 
 
 
-		var loadDescriptionColors = function() {
-			var descList = $('.feature-description');
 
-			if( !descList ) {
-				return;
-			}
-
-			for (var x = 0; x < descList.size(); x++ ) {
-				var desc = $(descList[x]),
-					
-				// Colors
-					background = desc.attr('data-background'),
-					shadow = desc.attr('data-shadow'),
-
-				// Conversions
-					insetShadow = 'inset 0 0 110px 5px ' + shadow + ', ',
-					boxShadow = '0 0 12px rgba(0,0,0,0.25)',
-					shadowCSSbase = insetShadow + boxShadow;
-
-				// Add CSS
-				desc.css("background-color", background);
-				desc.css("border-left-color", background);
-				desc.css("-webkit-box-shadow", shadowCSSbase);
-				desc.css("-moz-box-shadow", shadowCSSbase);
-				desc.css("box-shadow", shadowCSSbase);
-			}
-		};
-
+	/* Func: initializeSlider
+	 * Desc: Start the anythingSlider
+	 * Args: none
+	 */
+	 function initializeSlider() {
 
 		$('#slider').anythingSlider({
 			mode                : "f",   // Set mode to "horizontal", "vertical" or "fade" (only first letter needed); replaces vertical option
@@ -71,7 +42,7 @@
 			
 			onBeforeInitialize: loadDescriptionColors,
 
-			onInitialized: function(e, slider) { // Callback when the plugin finished initializing
+			onInitialized: function(e, slider) { // Callback when the plugin finishes initializing
 				var start = slider.options.startPanel;
 				// allow start & cloned panel images to load
 				// the rest get the src removed.
@@ -112,15 +83,68 @@
 			// clickControls       : "click focusin", // Events used to activate navigation control functionality
 			// clickSlideshow      : "click",         // Event used to activate slideshow play/stop button
 			// allowRapidChange    : false,           // If true, allow rapid changing of the active pane, instead of ignoring activity during animation
-		});
-		
+		});	
+
 
 		// Reset anythingslider on window resize
 		$(window).resize(function() {
 			$('#slider').anythingSlider();
 		});
 
-	});
+	}
+
+
+
+	/* Func: loadImg
+	 * Desc: Load slider images (only the first image is loaded with the page)
+	 * Args: none
+	 */
+	function loadImg (slider, page) {
+		slider.$items.eq(page).find('img').each(function() {
+			if ($(this).attr('src') === '') {
+				var newsrc = $(this).attr('data-src');
+				$(this).attr('src', newsrc);
+			}
+		});
+		$('#slider').anythingSlider();
+	}
+
+
+
+
+	/* Func: loadDescriptionColors
+	 * Desc: Set the CSS for the background of the description box
+	 * Args: none
+	 */
+	function loadDescriptionColors() {
+		var descList = $('.feature-description');
+
+		if( !descList ) {
+			return;
+		}
+
+		for (var x = 0; x < descList.size(); x++ ) {
+			var desc = $(descList[x]),
+				
+			// Colors
+				background = desc.attr('data-background'),
+				shadow = desc.attr('data-shadow'),
+
+			// Conversions
+				insetShadow = 'inset 0 0 110px 5px ' + shadow + ', ',
+				boxShadow = '0 0 12px rgba(0,0,0,0.25)',
+				shadowCSSbase = insetShadow + boxShadow;
+
+			// Add CSS
+			desc.css("background-color", background);
+			desc.css("border-left-color", background);
+			desc.css("-webkit-box-shadow", shadowCSSbase);
+			desc.css("-moz-box-shadow", shadowCSSbase);
+			desc.css("box-shadow", shadowCSSbase);
+		}
+	}
+
+
 
 
 }());
