31888d41c93b76568a7904d901986ab1a30ece6c
Nathan Walton <nwalton@byu.edu>
2013-09-24 17:59:12 -0600

    Add settings object to head for manipulating init.js

--- js/init.js
+++ js/init.js
@@ -10,20 +10,25 @@
     "use strict";
     Modernizr.load([ // Load jQuery first. If it doesn't make it from our location, get it from Google.
     {
-        load: "http://byuweb.github.io/Global-Assets/js/jquery-1.9.1.min.js",
+        test: typeof jQuery !== "undefined",
+        nope: BYUsettings.jQueryURL,
         complete: function() {
             if (!window.jQuery) {
-                Modernizr.load("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js");
+                Modernizr.load("//ajax.googleapis.com/ajax/libs/jquery/" + BYUsettings.jQueryVersion + "/jquery.min.js");
             }
         }
-    }, // Next load scripts that require jQuery. If touch is enabled, load alternate script file with touch support added.
+    }, // Next, load scripts that require jQuery. If touch is enabled, load alternate script file with touch support added.
     {
         test: Modernizr.touch,
         nope: "js/script.min.js",
         yep: "js/script-touch.min.js"
-    }, // If the variable "loadslider" is truthy, load the slider script
+    }, // If settings.loadslider is truthy, load the slider script
     {
-        test: loadslider,
+        test: BYUsettings.loadslider,
         yep: "js/slider.min.js"
+    }, // Add an array of url strings to the page settings object to load additional scripts
+    {
+        test: BYUsettings.loadScripts.length,
+        load: BYUsettings.loadScripts
     } ]);
 })();
